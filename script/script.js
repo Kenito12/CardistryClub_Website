386162b115d09af1bdfe3bf9635dc45553106168
Ken <nontsuke.ken@gmail.com>
2022-12-01 22:37:23 -0500

    working on CSS

--- script/script.js
+++ script/script.js
@@ -1,9 +1,8 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
+
+// fetch element with class name toogle-button and navbar-link when click and add class active with toggle behavior
 toggleButton.addEventListener('click', () =>{
     navbarLinks.classList.toggle('active')
 });
-
-console.log("working!!!!");
-alert.log("Hello");
